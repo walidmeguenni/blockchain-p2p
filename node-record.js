61a77df6d9950c6fd7be55dbed049f8d4be818af
walidmeguenni <walidmg125@gmail.com>
2023-04-19 15:41:39 +0100

    first commit

---
+++ node-record.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+
+const senders = ['ws://192.168.8.103:3002'];
+const wss = new WebSocket.Server({ port: 3000 });
+let portsender;
+wss.on('connection', (ws, req) => {
+  const senderIp =(req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace(/^::ffff:/, "");
+  console.log(`Connected to node: ${senderIp}`);
+
+  ws.on('message', async (message) => {
+    console.log(`Received data from node ${senderIp}: ${message}`);
+    portsender = message;
+    if (senders.length === 0) {
+      console.log("There is  no peer available for now");
+    } else {
+      ws.send(JSON.stringify(senders));
+    }
+    const address = `ws://${senderIp}:${message}`
+    const ipExists = senders.some((sender) => sender === address);
+    if (!ipExists) {
+      senders.push(address);
+    }
+
+    let filteredAddresses = senders.filter((addressIp) => addressIp !== address);
+    
+    ws.send(JSON.stringify(filteredAddresses))
+    console.log(senders);
+    filteredAddresses = []
+
+  });
+
+  ws.on('error', (err) => {
+    console.error(`Error connecting to node: ${senderIp}: ${err}`);
+  });
+
+  // Handle disconnects
+  ws.on('close', () => {
+    console.log(`Disconnected from node : ${senderIp}:${portsender}`);
+    portsender=''
+  });
+});
+
+wss.on('listening', () => {
+  const address = wss.address();
+  console.log(`Server listening on ${address.address}:${address.port}`);
+});
---
+++ utils/getPeers.js
@@ -0,0 +1,54 @@
+const WebSocket = require("ws");
+
+exports.GetPeers = (port) => {
+  return new Promise((resolve, reject) => {
+    const ws = new WebSocket("ws://192.168.8.103:3000");
+
+    ws.addEventListener("open", () => {
+      console.log("Connected to server");
+      ws.send(port);
+    });
+
+    ws.addEventListener("message", (event) => {
+      console.log("Received data from server:",event.data );
+      resolve(JSON.parse(event.data));
+      
+    });
+
+    ws.addEventListener("error", (error) => {
+      console.error("Error connecting to server:", error);
+      reject(error);
+    });
+
+    ws.addEventListener("close", () => {
+      console.log("Disconnected from server");
+    });
+
+  });
+};
+
+
+
+// const WebSocket = require("ws");
+// const ws = new WebSocket("ws://192.168.8.103:3000");
+// exports.GetPeers = async() => {
+//   let data;
+//   ws.addEventListener("open", () => {
+//     console.log("Connected to server");
+//   });
+
+//   ws.addEventListener("message", (event) => {
+//     data = JSON.parse(event.data);
+//     console.log("Received data from server:", data);
+//   });
+
+//   ws.addEventListener("error", (error) => {
+//     console.error("Error connecting to server:", error);
+//   });
+
+//   ws.addEventListener("close", () => {
+//     console.log("Disconnected from server");
+//   });
+
+//   return data;
+// };
